f99779b71259f84e7c651c4f81d6f55f5d49ad14
Orlov Nikita <nikitosina2388@gmail.com>
2024-04-19 14:46:43 +0300

    Initial commit
    This reverts commit ab3cbe13de54ae5e4da2de83659c3f8b360b2599.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Fitnes_system_Viking__by_Orlov__Timoshin_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+namespace Fitnes_system_Viking__by_Orlov__Timoshin_
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
