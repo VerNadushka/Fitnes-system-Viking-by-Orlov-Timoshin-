948314de2840d00b92b5b1c0083da4188e8b6cd2
c3developer <nikitosina2388@gmail.com>
2024-04-20 17:22:21 +0300

    Connect Sqlite and create file.db

---
+++ Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitnes_system_Viking__by_Orlov__Timoshin_
+{
+    internal class Class2
+    {
+        Database databaseObject = new Database();
+
+        /*
+         * INSERT INTO DATABASE
+         * 
+         */
+        string query = "INSERT INTO users";
+    }
+}
---
+++ Database.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+namespace Fitnes_system_Viking__by_Orlov__Timoshin_
+{
+    internal class Database
+    {
+        public SQLiteConnection myConnection;
+        public Database()
+        {
+            myConnection = new SQLiteConnection("DBfitnes.sqlite3");
+            if (File.Exists("./DBfitnes.sqlite3"))
+            {
+            SQLiteConnection.CreateFile("");
+
+            }
+        }
+    }
+}
